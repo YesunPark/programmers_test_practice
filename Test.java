f2cd6413070357537f58ef0ba966b42b20d5afea
YesunPark <lynn08082@gmail.com>
2023-03-26 17:59:51 +0900

    코테 입문 - 순서쌍의 개수

---
+++ Test.java
@@ -0,0 +1,34 @@
+// 23.03.26 2분 소요
+// 4시 55분 시작
+
+// 22.03.26 해결 못함
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i - 1] = i;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] * arr[j] == n) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(20));
+    }
+}
+
+
+
