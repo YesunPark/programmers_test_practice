2a77bbd99a49ff5f5c35e614a9b86387ce45c40c
YesunPark <lynn08082@gmail.com>
2023-04-19 23:28:53 +0900

    영어가 싫어요(진행 중)

---
+++ Level0/영어가싫어요/Main.java
@@ -0,0 +1,15 @@
+package Level0.영어가싫어요;
+// 23.04.19 진행중
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("onetwothreefourfivesixseveneightnine"));
+    }
+}
