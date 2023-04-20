3c41352a357704f82de9dfc9ab856e3daa2ee08b
YesunPark <lynn08082@gmail.com>
2023-04-20 22:11:12 +0900

    영어가 싫어요(15분 소요)

--- Level0/영어가싫어요/Main.java
+++ Level0/영어가싫어요/Main.java
@@ -1,9 +1,30 @@
 package Level0.영어가싫어요;
-// 23.04.19 진행중
+// 23.04.20 15분 소요  https://school.programmers.co.kr/learn/courses/30/lessons/120894
+
+import java.util.HashMap;
+
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        HashMap<String, String> map = new HashMap<>();
+        String[] nums = {
+                "zero", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine"
+        };
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i + "");
+        }
+
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < numbers.length(); i++) {
+            str.append(numbers.split("")[i]);
+            if (map.get(str.toString()) != null) {
+                answer.append(map.get(str.toString()));
+                str = new StringBuilder();
+            }
+        }
+
+        return Long.parseLong(answer.toString());
     }
 }
 
