2fcfc762791b14223968f7ce47ef6d397d41bf87
YesunPark <lynn08082@gmail.com>
2023-04-28 00:14:09 +0900

    문자열 계산하기(30분)

--- Level0/문자열계산하기/Main.java
+++ Level0/문자열계산하기/Main.java
@@ -1,34 +1,40 @@
 package Level0.문자열계산하기;
-// 23.04.27 15분 소요중(해결 중)
+
+// 23.04.27 30분 소요
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
 
-
         int idx = 0;
-        while (idx < my_string.length()) {
 
-            String cal = "";
-            for (String str : my_string.split("")) {
-                StringBuilder number = new StringBuilder();
-                if (!"".equals(number.toString()) && !"".equals(cal)) {
-
-                }
-
-                if ("".equals(str)) {
-                    continue;
-                } else if ("+".equals(str) || "-".equals(str)) {
-                    cal = str;
-                } else {
-                    number.append(str);
-                }
-
-                idx++;
+        String cal = "";
+        StringBuilder number = new StringBuilder();
+        for (String str : my_string.split("")) {
+            if (" ".equals(str)) {
+                continue;
             }
 
+            if ("+".equals(str) || "-".equals(str)) {
+                if ("".equals(cal)) {
+                    answer = Integer.parseInt(number.toString());
+                } else if ("+".equals(cal)) {
+                    answer += Integer.parseInt(number.toString());
+                } else {
+                    answer -= Integer.parseInt(number.toString());
+                }
 
+                cal = str;
+                number = new StringBuilder();
+            } else {
+                number.append(str);
+            }
         }
 
+        if ("+".equals(cal)) {
+            answer += Integer.parseInt(number.toString());
+        } else {
+            answer -= Integer.parseInt(number.toString());
+        }
 
         return answer;
     }
