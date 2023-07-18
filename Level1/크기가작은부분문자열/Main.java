d14a01b0cd2182c3e657d16f42c392f72647942d
YesunPark <lynn08082@gmail.com>
2023-07-18 23:55:20 +0900

    Lv1. 크기가 작은 부분 문자열(20분)

--- Level1/크기가작은부분문자열/Main.java
+++ Level1/크기가작은부분문자열/Main.java
@@ -1,11 +1,21 @@
 package Level1.크기가작은부분문자열;
 //https://school.programmers.co.kr/learn/courses/30/lessons/147355
-// 23.07.18
+// 23.07.18 20분
 
 class Solution {
 
     public int solution(String t, String p) {
         int answer = 0;
+        int pLen = p.length();
+
+        for (int i = 0; i < t.length() - pLen + 1; i++) {
+            long num = Long.parseLong(t.substring(i, (i + pLen)));
+
+            if ((num <= Long.parseLong(p))) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
@@ -14,6 +24,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("3141592", "271"));
+        System.out.println(s.solution("500220839878", "7"));
     }
 }
