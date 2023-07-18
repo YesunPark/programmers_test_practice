8f41988154949f6131c7f93e6752a3c694682383
YesunPark <lynn08082@gmail.com>
2023-07-18 23:37:07 +0900

    크기가 작은 부분 문자열(해결 중)

---
+++ Level1/크기가작은부분문자열/Main.java
@@ -0,0 +1,19 @@
+package Level1.크기가작은부분문자열;
+//https://school.programmers.co.kr/learn/courses/30/lessons/147355
+// 23.07.18
+
+class Solution {
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("3141592", "271"));
+    }
+}
