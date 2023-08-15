05b8842e30a243bbefc9cd27dd6c8576e83acc09
YesunPark <lynn08082@gmail.com>
2023-08-15 22:44:11 +0900

    Lv.1 수박수박수박수박수박수? (3분)

---
+++ Level1/수박수박수박수박수박수/Main.java
@@ -0,0 +1,29 @@
+package Level1.수박수박수박수박수박수;
+// https://school.programmers.co.kr/learn/courses/30/lessons/12922
+// 23.08.15 3분
+
+class Solution {
+
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder repeat = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                repeat.append("박");
+            } else {
+                repeat.append("수");
+            }
+        }
+
+        return answer.toString();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(3));
+    }
+}
